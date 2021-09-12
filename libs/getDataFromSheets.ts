ada4d1b17b8f27b586cb2fdb2943ea6bb0ed6c2d
Murillo Teixeira <murillo.teixeira@usp.br>
2021-09-12 14:03:00 -0300

    add: comunication with google sheets

--- libs/getDataFromSheets.ts
+++ libs/getDataFromSheets.ts
@@ -1,19 +1,22 @@
 import { google } from "googleapis";
 
 export async function getRows() {
-  const auth = await google.auth.getClient({
-    scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-  });
-
-  const sheets = google.sheets({ version: "v4", auth });
+  const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+  const jwt = new google.auth.JWT(
+    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    undefined,
+    (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+    target
+  );
 
   const range = `NewsBek`;
-
+  const sheets = google.sheets({ version: "v4", auth: jwt });
   const response = await sheets.spreadsheets.values.get({
     spreadsheetId: process.env.SHEET_ID,
-    range,
+    range, // sheet name
   });
 
+  // @ts-ignore
   const rows = response.data.values;
   rows?.shift();
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,61 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Link from "next/link";
+import { getRows } from "../libs/getDataFromSheets";
+import { google } from "googleapis";
 
-const Home: NextPage = () => {
+export async function getServerSideProps() {
+  const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+  const jwt = new google.auth.JWT(
+    process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    undefined,
+    (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+    target
+  );
+
+  const range = `NewsBek`;
+  const sheets = google.sheets({ version: "v4", auth: jwt });
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range, // sheet name
+  });
+
+  // @ts-ignore
+  const rows = response.data.values;
+  rows?.shift();
+
+  return {
+    props: {
+      rows,
+    },
+  };
+}
+
+const Home: NextPage = ({ rows }: any) => {
   return (
     <>
       <Head>
-        <title>Rateria Poli-USP</title>
+        <title>Newsbek</title>
         <meta name="description" content="A bateria da Escola Politécnica" />
         <link rel="icon" href="/images/logo-fundo-azul.svg" />
       </Head>
       <Header />
       <main className={styles.mainContent}>
         <article>
-          {/* {rows.map((row: any) => {
+          {rows.map((row: any) => {
             const adress = "/newsbek/" + (rows.indexOf(row) + 1);
-            return ( */}
-          <div className={styles.card}>
-            <Link href="/posts/1">
-              <a>Título do post</a>
-            </Link>
-            <h3>01/01/2022</h3>
-          </div>
-          {/* );
-          })} */}
+            return (
+              <div key={row[0]} className={styles.card}>
+                <Link href={adress}>
+                  <a>{row[0]}</a>
+                </Link>
+                <h3>{row[2]}</h3>
+              </div>
+            );
+          })}
         </article>
       </main>
     </>
