bd3e2c5dcf4966db4fcf4fab5dba51ab455a69e5
Murillo Teixeira <murillo.teixeira@usp.br>
2021-09-13 14:12:12 -0300

    fix: ts error

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 
 import logo from '../../public/images/logo-fundo-azul.svg';
 
-const shimmer = (w, h) => `
+const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
