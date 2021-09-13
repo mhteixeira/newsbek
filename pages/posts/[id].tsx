68fc906850abbd10988d8c1dccef9ac655fd74be
Murillo Teixeira <murillo.teixeira@usp.br>
2021-09-12 21:18:12 -0300

    Revert "feat: fallback true on posts paths"
    This reverts commit 5b10758fe36b38feea1a2b9783384bcba4145b97.

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -75,7 +75,7 @@ export async function getStaticPaths() {
   // We'll pre-render only these paths at build time.
   // { fallback: blocking } will server-render pages
   // on-demand if the path doesn't exist.
-  return { paths, fallback: "true" };
+  return { paths, fallback: "blocking" };
 }
 
 export default function Post({ title, content, date }: any) {
