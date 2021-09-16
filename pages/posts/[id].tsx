644bda01fa9b5ddf589cfce40c500584650ffb01
Murillo Teixeira <murillo.teixeira@usp.br>
2021-09-15 22:19:28 -0300

    fix: next images without blur

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -14,7 +14,6 @@ const MyImage = (props: any) => {
       alt={"next/image"}
       src={props.src}
       layout="responsive"
-      placeholder="blur"
       width={600}
       height={400}
     />
